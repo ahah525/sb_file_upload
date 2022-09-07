7f4798542ba0599f79d19d217624b421b9ba229a
ahah525 <ahah525@naver.com>
2022-09-07 15:26:18 +0900

    test : MockMvc를 이용해 HomeController  테스트 수행

---
+++ src/test/java/com/ll/exam/app10/app/home/controller/HomeControllerTest.java
@@ -0,0 +1,38 @@
+package com.ll.exam.app10.app.home.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class HomeControllerTest {
+    @Autowired
+    private MockMvc mvc;    // 브라우저
+
+    @Test
+    void main() throws Exception {
+        // when
+        ResultActions resultActions = mvc
+                .perform(get("/"))
+                .andDo(print());
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(HomeController.class))
+                .andExpect(handler().methodName("main"))
+                .andExpect(content().string(containsString("안녕")));
+    }
+}
