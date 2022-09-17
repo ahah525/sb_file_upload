33685e67ee460d85f77874d7f5a0c2a937167a1c
ahah525 <ahah525@naver.com>
2022-09-17 23:40:36 +0900

    feat : 카카오 로그인 처리

--- src/main/java/com/ll/exam/app10/app/security/service/OAuth2UserService.java
+++ src/main/java/com/ll/exam/app10/app/security/service/OAuth2UserService.java
@@ -1,45 +1,72 @@
 package com.ll.exam.app10.app.security.service;
 
+import com.ll.exam.app10.app.member.domain.Member;
+import com.ll.exam.app10.app.member.exception.MemberNotFoundException;
+import com.ll.exam.app10.app.member.repository.MemberRepository;
+import com.ll.exam.app10.app.security.dto.MemberContext;
+import com.ll.exam.app10.app.security.exception.OAuthTypeMatchNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 // TODO : 복붙 파일
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OAuth2UserService extends DefaultOAuth2UserService {
-    /*
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Override
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
+
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint()
                 .getUserNameAttributeName();
         Map<String, Object> attributes = oAuth2User.getAttributes();
+
         String oauthId = oAuth2User.getName();
+
         Member member = null;
         String oauthType = userRequest.getClientRegistration().getRegistrationId().toUpperCase();
+
         if (!"KAKAO".equals(oauthType)) {
             throw new OAuthTypeMatchNotFoundException();
         }
+
         if (isNew(oauthType, oauthId)) {
             switch (oauthType) {
                 case "KAKAO" -> {
                     Map attributesProperties = (Map) attributes.get("properties");
                     Map attributesKakaoAcount = (Map) attributes.get("kakao_account");
                     String nickname = (String) attributesProperties.get("nickname");
+                    // email 은 선택 동의 항목
                     String email = "%s@kakao.com".formatted(oauthId);
+                    // oauthId: 카카오가 부여하는 id
                     String username = "KAKAO_%s".formatted(oauthId);
+
                     if ((boolean) attributesKakaoAcount.get("has_email")) {
                         email = (String) attributesKakaoAcount.get("email");
                     }
+
                     member = Member.builder()
                             .email(email)
                             .username(username)
-                            .password(passwordEncoder.encode(UUID.randomUUID().toString()))
+                            .password("")
                             .build();
+
                     memberRepository.save(member);
                 }
             }
@@ -47,15 +74,17 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
             member = memberRepository.findByUsername("%s_%s".formatted(oauthType, oauthId))
                     .orElseThrow(MemberNotFoundException::new);
         }
-        Set<GrantedAuthority> authorities = new LinkedHashSet<>();
-        authorities.add(new SimpleGrantedAuthority("USER"));
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("member"));
         return new MemberContext(member, authorities, attributes, userNameAttributeName);
     }
-    */
 
-    /*
+    /***
+     * 최초 로그인 : true(회원가입 필요)
+     * 최초 로그인 X : false(회원가입 필요X)
+     */
     private boolean isNew(String oAuthType, String oAuthId) {
         return memberRepository.findByUsername("%s_%s".formatted(oAuthType, oAuthId)).isEmpty();
     }
-    */
 }
