feee562f11d2f0feeb05ad44258520de520025eb
ahah525 <ahah525@naver.com>
2022-09-17 21:46:28 +0900

    feat : @AuthenticationPrincipal MemberContext 로 로그인한 회원 정보 얻기

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -1,8 +1,9 @@
 package com.ll.exam.app10.app.home.controller;
 
 import com.ll.exam.app10.app.member.service.MemberService;
+import com.ll.exam.app10.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,10 +25,17 @@ public class HomeController {
         return "home/about";
     }
 
-    @PreAuthorize("isAuthenticated()")
     @GetMapping("/currentUserOrigin")
     @ResponseBody
     public Principal currentUserOrigin(Principal principal) {
+        // principal : name 만 얻을 수 있음
         return principal;
+    }
+
+    @GetMapping("/currentUser")
+    @ResponseBody
+    public MemberContext currentUser(@AuthenticationPrincipal MemberContext memberContext) {
+        // memberContext: username, id, profileImgUrl 등 모두 얻을 수 있음
+        return memberContext;
     }
 }
